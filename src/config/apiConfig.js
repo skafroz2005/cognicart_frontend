ec1b2ed28d4846e696766da463bd2c34ea98fbb0
Afroz <skafroz5767@gmail.com>
2026-03-30 18:27:46 +0530

    updated backend URL

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+// export const API_BASE_URL = "http://localhost:5454";
 
+export const API_BASE_URL = "https://cognicart-backend.onrender.com" || "http://localhost:5454";
 export const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
